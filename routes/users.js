954de81f5c803f74926bbcd95f40b7c5b7f603dc
Julie Rivest <julierivest93@gmail.com>
2017-07-20 22:51:26 +0000

    added profile endpoint, and other things

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:id', function(req, res, next) {
+  res.render('profile', {
+    title: '',
+    id: req.params.id,
+  })
+});
+
 module.exports = router;
