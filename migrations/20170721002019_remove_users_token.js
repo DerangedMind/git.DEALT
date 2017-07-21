82036f0ad3f3aecb5b15c98691b947403e9972af
Riki <rick.libby.91@gmail.com>
2017-07-21 00:33:05 +0000

    remove token column of users

---
+++ migrations/20170721002019_remove_users_token.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('users', function(table) {
+      table.dropColumn('token')
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('users', function(table) {
+      table.string('token').nullable()
+    })
+  ])
+};
