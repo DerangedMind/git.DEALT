49b0e3264b853acc1f96e8a482119d32d310c4cb
Riki <rick.libby.91@gmail.com>
2017-07-21 20:45:08 +0000

    log env in knexserver for debugging

--- knexserver.js
+++ knexserver.js
@@ -1,7 +1,18 @@
 require('dotenv').config()
 
 const config = require('./knexfile')
+
 const env = process.env.DATABASE_URL ? 'production' : 'development'
+console.log(`
+
+
+
+  env
+
+
+
+
+  `)
 const knex = require('knex')(config[env])
 
 module.exports = knex
