5d1dd13e8962488b68b24d8547014620503ddbe1
Julie Rivest <julierivest93@gmail.com>
2017-07-21 02:38:45 +0000

    near-functional username edit button

---
+++ public/scripts/edit-username.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".edit-icon").click(function() {
+    $(".user-name").attr("contenteditable", "true").focus();
+  });
+});
+
+//SHOULD BOX AROUND NAME BE THERE OR ONLY WHEN THEY CLICK EDIT
+//HOW WOULD THEY SAVE?? CMD+ENTER?? SAVE/CANCEL ICONS APPEAR NEXT TO/INSTEAD OF EDIT??
+//CANCEL JUST BY CLICKING ELSEWHERE ON SCREEN???
