8b014a5c1df981e7acee6fe0cc83ceb9c12c70a7
Riki <rick.libby.91@gmail.com>
2017-07-21 20:33:13 +0000

    update knex environment variables

--- knexserver.js
+++ knexserver.js
@@ -1,3 +1,7 @@
-const knex = require('knex')(require('./knexfile').development)
+require('dotenv').config()
+
+const config = require('./knexfile')
+const env = process.env.DATABASE_URL ? 'production' : 'development'
+const knex = require('knex')(config[env])
 
 module.exports = knex
