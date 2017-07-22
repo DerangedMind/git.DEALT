4f559ba2021d9310cf625ffffc271e6ad9b2566f
Riki <rick.libby.91@gmail.com>
2017-07-22 21:32:24 +0000

    select card and submit button are working, sending post request to /gops

--- public/scripts/gops-ajax.js
+++ public/scripts/gops-ajax.js
@@ -1,20 +1,33 @@
 $(() => {
 
-  $('#submitCard').on('click', function (event) {
+  $('#submit-card').on('click', submitCard)
+
+  $('.table li').on('click', function (event) {
+    $('.selected').removeClass('selected')
+    $(event.target).parents('li').addClass('selected')
+    console.log($('.selected .rank').text())
+  })
+
+  function submitCard(event) {
     event.preventDefault()
-    submitCard($('#selectedCard'))
-  })
-
-  $('.card').on('click', function (event) {
-    $(event.target).toggleClass('selected')
-  })
-
-  function submitCard(card) {
-    $.ajax('/gops', {
-
-    }
-
+    
+    let card = $('.selected .rank').text()
+    console.log(card)
+    $.ajax({
+      url: '/gops', 
+      method: 'POST',
+      data: {
+        card: card
+      }
     })
-  }
+      .done(function (response) {
+        $('#submit-card')
+          .off('click', submitCard)
+          .prop('disabled', true)
+      })
+      .fail(function (err) {
 
-}
+      })
+    }
+  }
+)
