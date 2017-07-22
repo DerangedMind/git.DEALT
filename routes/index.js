decdb23cb02c92ba8f9ec32f7d887a7d7d47ae3a
Julie Rivest <julierivest93@gmail.com>
2017-07-22 17:57:42 +0000

    adding item box in queue

---
+++ routes/index.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+
+module.exports = function(app, passport) {
+
+  /* GET home page. */
+  router.get('/', function(req, res, next) {
+    res.render('index', {
+      title: 'LCG Castle',
+      message: req.flash('loginMessage')
+    })
+  })
+
+  router.get('/lobby', isLoggedIn, function(req, res, next) {
+    res.render('lobby', {
+      title: ''
+    })
+  })
+
+  router.get('/create', isLoggedIn, function(req, res, next) {
+    res.render('create', {
+      title: ''
+    })
+  })
+
+router.get('/users/:id', function(req, res, next) {
+  res.render('profile', {
+    title: '',
+    id: req.params.id,
+  })
+});
+
+
+  router.get('/auth/facebook',
+              passport.authenticate('facebook', {
+                scope:'email'
+              }
+            ))
+  router.get('/auth/facebook/callback',
+              passport.authenticate('facebook', {
+                successRedirect: '/lobby',
+                failureRedirect: '/'
+                }
+            ))
+  router.get('/logout', isLoggedIn, function(req, res, next) {
+    req.logout()
+    res.redirect('/')
+  })
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+
+    res.redirect('/')
+  }
+
+  return router
+}
