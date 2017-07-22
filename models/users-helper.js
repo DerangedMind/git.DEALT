313a356ae289d5205d4dc56d9d4ae90d559d2525
Riki <rick.libby.91@gmail.com>
2017-07-22 17:39:31 +0000

    ensure facebook login succeeds when email doesn't exist on fb profile

--- models/users-helper.js
+++ models/users-helper.js
@@ -25,6 +25,9 @@ module.exports = {
               else {
                 console.log(profile)
                 console.log(profile.emails)
+                // sometimes facebook members do not have
+                //  a verified email address, so 
+                //  this ensures they can still create an account.
                 let email = null
                 if (profile.emails !== undefined) {
                   email = profile.emails[0].value
