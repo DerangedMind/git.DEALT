1c0597b21f3fa486d365af2a5dcbb72088b4263f
Riki <rick.libby.91@gmail.com>
2017-07-24 04:58:34 +0000

    uncomment endRound and add console log

--- routes/routes.js
+++ routes/routes.js
@@ -156,13 +156,13 @@ module.exports = function(app, passport) {
   })
 
   router.get('/gops/:game_id/ready_check', isLoggedIn, function(req, res, next) {
-    res.send(true)
+    
   })
 
   router.post('/gops/:game_id', isLoggedIn, function(req, res, next) {
     console.log('testing post')
     gopsgame.playCard(req.params.game_id, req.session.passport.user[0].id, req.body.card);
-    // gopsgame.endRound(req.params.game_id);
+    gopsgame.endRound(req.params.game_id);
     // gopsgame.getGameWinner(req.params.game_id);
     res.send(200)
 
